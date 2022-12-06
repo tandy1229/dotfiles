c69ac76e116caf43884bf4d330914ef7aed57e05
tandy <tandy1229@qq.com>
2022-12-06 18:50:30 +0800

    feat: add gnufind and ncurses(terminfo) to the path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,7 +1,10 @@
 PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+PATH="/usr/local/opt/findutils/libexec/gnubin:$PATH"
 PATH="/usr/local/opt/gnu-which/libexec/gnubin:$PATH"
 PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
 PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
 PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"
 PATH="/usr/local/opt/curl/bin:$PATH"
+# terminfo
+PATH="/usr/local/opt/ncurses/bin:$PATH"
 # PATH="/usr/local/opt/node@14/bin:$PATH"
