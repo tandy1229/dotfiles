6744b8f6f82731c710481f79c1437fc6cc3cae19
tandy <tandy1229@qq.com>
2023-07-08 23:44:41 +0800

    feat: update the homebrew domain

--- zsh/go.zsh
+++ zsh/go.zsh
@@ -1,4 +1,8 @@
 export GOPATH="/Users/yitaimin/go"
-export GOROOT="/usr/local/Cellar/go/1.19.3/libexec"
+# export GOROOT="/usr/local/Cellar/go/1.20.3/libexec"
+# export GOROOT="/usr/local/go"
 export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:$GOROOT/bin
+# export GO15VENDOREXPERIMENT=1
+export GO15VENDOREXPERIMENT=1
+# export GO111MODULE="off"
