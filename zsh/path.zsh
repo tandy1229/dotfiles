4c8706911f873d3397c4616aa0b7562768d1c4f7
tandy <tandy1229@qq.com>
2023-08-29 10:55:50 +0800

    feat(zsh): add path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -4,6 +4,7 @@ PATH="/usr/local/opt/gnu-which/libexec/gnubin:$PATH"
 PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
 PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
 PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"
+PATH="/usr/local/opt/gawk/libexec/gnubin:$PATH"
 PATH="/usr/local/opt/curl/bin:$PATH"
 # terminfo
 PATH="/usr/local/opt/ncurses/bin:$PATH"
