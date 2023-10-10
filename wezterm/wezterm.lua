cf77d03789adb5a773db94f3c0e0c77bf3e60e09
tandy <tandy1229@qq.com>
2023-10-10 15:55:24 +0800

    feat(wezterm): change Chinese font to SiYuan HeiTi

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,51 +1,53 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 --- @class config
 local config = wezterm.config_builder and wezterm.config_builder() or {}
 
 -- config.color_scheme = 'Tokyo Night Moon'
 local function scheme_for_appearance(appearance)
-  if appearance:find("Dark") then
-    -- Dark theme
-    return "Tokyo Night Moon"
-  else
-    -- Light theme
-    return "Dracula"
-  end
+	if appearance:find("Dark") then
+		-- Dark theme
+		return "Tokyo Night Moon"
+	else
+		-- Light theme
+		return "Dracula"
+	end
 end
 
 config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 
-config.font = wezterm.font_with_fallback {
-  'JetBrainsMono Nerd Font',
-  'PingFang',
-  'Microsoft YaHei',
-  'Noto Color Emoji',
+config.font = wezterm.font_with_fallback({
+	"JetBrainsMono Nerd Font",
+	-- 'Source Han Serif SC',
+	"Source Han Sans SC",
+	"Microsoft YaHei",
+	"Noto Color Emoji",
+})
+
+config.font_rules = {
+	{
+		intensity = "Bold",
+		italic = false,
+		font = wezterm.font_with_fallback({
+			{ family = "JetBrainsMono Nerd Font", weight = "ExtraBold" },
+			{ family = "Source Han Sans SC",      weight = "Bold" },
+			-- { family = "Source Han Serif SC",     weight = "Bold" },
+			{ family = "Microsoft YaHei",         weight = "Bold" },
+		}),
+	},
+	{
+		intensity = "Bold",
+		italic = true,
+		font = wezterm.font_with_fallback({
+			{ family = "JetBrainsMono Nerd Font", style = "Italic", weight = "ExtraBold" },
+			{ family = "Source Han Sans SC",      style = "Italic", weight = "Bold" },
+			-- { family = "Source Han Serif SC",     style = "Italic", weight = "Bold" },
+			{ family = "Microsoft YaHei",         style = "Italic", weight = "Bold" },
+		}),
+	},
 }
 
-config.font_rules =
-{
-  {
-    intensity = 'Bold',
-    italic = false,
-    font = wezterm.font_with_fallback {
-      { family = 'JetBrainsMono Nerd Font', weight = 'ExtraBold' },
-      { family = 'PingFang',                weight = 'Bold' },
-      { family = 'Microsoft YaHei',         weight = 'Bold' },
-    },
-  },
-  {
-    intensity = 'Bold',
-    italic = true,
-    font = wezterm.font_with_fallback {
-      { family = 'JetBrainsMono Nerd Font', style = 'Italic', weight = 'ExtraBold' },
-      { family = 'PingFang',                style = 'Italic', weight = 'Bold' },
-      { family = 'Microsoft YaHei',         style = 'Italic', weight = 'Bold' },
-    },
-  },
-}
-
-config.harfbuzz_features = { 'zero' }
+config.harfbuzz_features = { "zero" }
 
 config.font_size = 18
 
